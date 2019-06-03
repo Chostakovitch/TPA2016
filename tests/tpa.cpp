366285208977266f646b1a4f25d59daaa475cc65
Quentin Duchemin <quentinduchemin@tuta.io>
2019-06-03 14:58:42 +0000

    Add gain tests and change REQUIRE to CHECK
    Indeed, CHECK is better suited for TDD-like, as we write tests and check that all of them fail before writing effective code. REQUIRE stops the tests on failure and does not allow to check the absence of false positive

--- tests/tpa.cpp
+++ tests/tpa.cpp
@@ -7,48 +7,48 @@ SCENARIO("Amplifier default values are expected") {
 		I2C_TPA2016 tpa(1);
 		WHEN("The amplifier starts") {
 			THEN("Channels are enabled") {
-				REQUIRE(tpa.rightEnabled());
-				REQUIRE(tpa.leftEnabled());
+				CHECK(tpa.rightEnabled());
+				CHECK(tpa.leftEnabled());
 			}
 			THEN("Amplifier is ready") {
-				REQUIRE(tpa.ready());
+				CHECK(tpa.ready());
 			}
 			THEN("No short circuit on left and right amplifier") {
-				REQUIRE(!tpa.rightShorted());
-				REQUIRE(!tpa.leftShorted());
+				CHECK(!tpa.rightShorted());
+				CHECK(!tpa.leftShorted());
 			}
 			THEN("Circuit is not too hot") {
-				REQUIRE(!tpa.tooHot());
+				CHECK(!tpa.tooHot());
 			}
 			THEN("Noise gate is enabled") {
-				REQUIRE(tpa.noiseGateEnabled());
+				CHECK(tpa.noiseGateEnabled());
 			}
 			THEN("Attack time is 6.4ms/6dB") {
-				REQUIRE(tpa.attackTime() == 5);
+				CHECK(tpa.attackTime() == 5);
 			}
 			THEN("Release time is 1.81s/6dB") {
-				REQUIRE(tpa.releaseTime() == 11);
+				CHECK(tpa.releaseTime() == 11);
 			}
 			THEN("Hold time is disabled") {
-				REQUIRE(tpa.holdTime() == 0);
+				CHECK(tpa.holdTime() == 0);
 			}
 			THEN("Fixed gain is 6dB") {
-				REQUIRE(tpa.gain() == 6);
+				CHECK(tpa.gain() == 6);
 			}
 			THEN("Limiter is enabled") {
-				REQUIRE(tpa.limiterEnabled());
+				CHECK(tpa.limiterEnabled());
 			}
 			THEN("NoiseGate Treshold is 4mV RMS") {
-				REQUIRE(tpa.noiseGateThreshold() == 1);
+				CHECK(tpa.noiseGateThreshold() == 1);
 			}
 			THEN("Output limiter level is 6.5dBV") {
-				REQUIRE(tpa.limiterLevel() == 26);
+				CHECK(tpa.limiterLevel() == 26);
 			}
 			THEN("Max gain is 30dB") {
-				REQUIRE(tpa.maxGain() == 12);
+				CHECK(tpa.maxGain() == 12);
 			}
 			THEN("Compression ratio is 4:1") {
-				REQUIRE(tpa.compressionRatio() == TPA2016_AGC_4);
+				CHECK(tpa.compressionRatio() == TPA2016_AGC_4);
 			}
 		}
 	}
@@ -61,31 +61,31 @@ SCENARIO("Toggle-bit features") {
 		WHEN("The right and the left channel are turned off then on") {
 			tpa.enableChannels(false, false);
 			THEN("Their status change to off") {
-				REQUIRE(!tpa.rightEnabled());
-				REQUIRE(!tpa.leftEnabled());
+				CHECK(!tpa.rightEnabled());
+				CHECK(!tpa.leftEnabled());
 			}
 			tpa.enableChannels(true, true);
 			THEN("Their status change to on") {
-				REQUIRE(tpa.rightEnabled());
-				REQUIRE(tpa.leftEnabled());
+				CHECK(tpa.rightEnabled());
+				CHECK(tpa.leftEnabled());
 			}
 		}
 
 		WHEN("The right channel is turned on and the left channel is turned off") {
 			tpa.enableChannels(true, false);
 			THEN("The right channel is on and the left channel is off") {
-				REQUIRE(tpa.rightEnabled());
-				REQUIRE(!tpa.leftEnabled());
+				CHECK(tpa.rightEnabled());
+				CHECK(!tpa.leftEnabled());
 			}
 		}
 		WHEN("We request software shutdown") {
 			tpa.softwareShutdown(true);
 			THEN("Amplifier is not ready anymore") {
-				REQUIRE(!tpa.ready());
+				CHECK(!tpa.ready());
 				WHEN("Software shutdown is disabled") {
 					tpa.softwareShutdown(false);
 					THEN("Amplifier becomes ready") {
-						REQUIRE(tpa.ready());
+						CHECK(tpa.ready());
 					}
 				}
 			}
@@ -93,19 +93,19 @@ SCENARIO("Toggle-bit features") {
 		WHEN("We reset short circuit status") {
 			tpa.resetShort(true, true);
 			THEN("Status remains unshorted") {
-				REQUIRE(!tpa.rightShorted());
-				REQUIRE(!tpa.leftShorted());
+				CHECK(!tpa.rightShorted());
+				CHECK(!tpa.leftShorted());
 			}
 		}
 
 		WHEN("Noise gate is turned on") {
 			tpa.enableNoiseGate(true);
 			THEN("Noise gate is enabled") {
-				REQUIRE(tpa.noiseGateEnabled());
+				CHECK(tpa.noiseGateEnabled());
 				WHEN("Noise gate is turned off") {
 					tpa.enableNoiseGate(false);
 					THEN("Noise gate is disabled") {
-						REQUIRE(!tpa.noiseGateEnabled());
+						CHECK(!tpa.noiseGateEnabled());
 					}
 				}
 			}
@@ -125,18 +125,18 @@ SCENARIO("Attack time modification") {
 		WHEN("We set an attack time of 1.28ms/6dB") {
 			tpa.setAttackTime(1.28);
 			THEN("Attack time should report 1.28ms/6dB") {
-				REQUIRE(tpa.attackTime() == 1.28f);
+				CHECK(tpa.attackTime() == 1.28f);
 			}
 		}
 		WHEN("We set an attack time of 3ms/6dB (non TPA2016_ATTACK_STEP multiple)") {
 			tpa.setAttackTime(3);
 			THEN("Attack time should report 2.56ms/6dB") {
-				REQUIRE(tpa.attackTime() == 2.56f);
+				CHECK(tpa.attackTime() == 2.56f);
 			}
 		}
 		WHEN("We set an attack time of 200ms/6dB (invalid value)") {
 			THEN("An out-of-range exception should be thrown") {
-				REQUIRE_THROWS_AS(tpa.setAttackTime(200), std::out_of_range);
+				CHECK_THROWS_AS(tpa.setAttackTime(200), std::out_of_range);
 			}
 		}
 	}
@@ -148,18 +148,18 @@ SCENARIO("Release time modification") {
 		WHEN("We set an release time of 0.1644sec/6dB") {
 			tpa.setReleaseTime(0.1644);
 			THEN("Release time should report 0.1644sec/6dB") {
-				REQUIRE(tpa.releaseTime() == 0.1644f);
+				CHECK(tpa.releaseTime() == 0.1644f);
 			}
 		}
 		WHEN("We set an release time of 4sec/6dB (non TPA2016_RELEASE_STEP multiple)") {
 			tpa.setReleaseTime(4);
 			THEN("Release time should report 3.9456sec/6dB") {
-				REQUIRE(tpa.releaseTime() == 3.9456f);
+				CHECK(tpa.releaseTime() == 3.9456f);
 			}
 		}
 		WHEN("We set an release time of 12sec/6dB (invalid value)") {
 			THEN("An out-of-range exception should be thrown") {
-				REQUIRE_THROWS_AS(tpa.setReleaseTime(12), std::out_of_range);
+				CHECK_THROWS_AS(tpa.setReleaseTime(12), std::out_of_range);
 			}
 		}
 	}
@@ -171,18 +171,64 @@ SCENARIO("Hold time modification") {
 		WHEN("We set an hold time of 0.0274sec/step") {
 			tpa.setHoldTime(0.0274);
 			THEN("Hold time should report 0.0274sec/step") {
-				REQUIRE(tpa.holdTime() == 0.0274f);
+				CHECK(tpa.holdTime() == 0.0274f);
 			}
 		}
 		WHEN("We set an hold time of 0.05sec/step (non TPA2016_HOLD_STEP multiple)") {
 			tpa.setHoldTime(0.05);
 			THEN("Hold time should report 0.0411sec/step") {
-				REQUIRE(tpa.holdTime() == 0.0411f);
+				CHECK(tpa.holdTime() == 0.0411f);
 			}
 		}
 		WHEN("We set an hold time of 2sec/step (invalid value)") {
 			THEN("An out-of-range exception should be thrown") {
-				REQUIRE_THROWS_AS(tpa.setHoldTime(2), std::out_of_range);
+				CHECK_THROWS_AS(tpa.setHoldTime(2), std::out_of_range);
+			}
+		}
+	}
+}
+
+SCENARIO("Maximum gain modification") {
+	GIVEN("An I2C connection on bus 1") {
+		I2C_TPA2016 tpa(1);
+		WHEN("We set the maximum gain to 18dB") {
+			tpa.setMaxGain(18);
+			THEN("Maximum gain should report 18dB") {
+				CHECK(tpa.maxGain() == 18);
+			}
+		}
+		WHEN("We set the maximum gain to 25.5dB") {
+			tpa.setMaxGain(25.5);
+			THEN("Maximum gain should report 25dB") {
+				CHECK(tpa.maxGain() == 25);
+			}
+		}
+		WHEN("We set the maximum gain to 31dB (invalid value)") {
+			THEN("An out-of-range exception should be thrown") {
+				CHECK_THROWS_AS(tpa.setMaxGain(31), std::out_of_range);
+			}
+		}
+	}
+}
+
+SCENARIO("Fixed gain modification") {
+	GIVEN("An I2C connection on bus 1") {
+		I2C_TPA2016 tpa(1);
+		WHEN("We set the gain to -28dB") {
+			tpa.setGain(-28);
+			THEN("Gain should report 18dB") {
+				CHECK(tpa.gain() == -28);
+			}
+		}
+		WHEN("We set the gain to 25.5dB") {
+			tpa.setGain(25.5);
+			THEN("Gain should report 25dB") {
+				CHECK(tpa.gain() == 25);
+			}
+		}
+		WHEN("We set the gain to 31dB (invalid value)") {
+			THEN("An out-of-range exception should be thrown") {
+				CHECK_THROWS_AS(tpa.setGain(31), std::out_of_range);
 			}
 		}
 	}
